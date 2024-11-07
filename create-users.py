f5528ca60e43e4bb4391632158b8b89cf290b6de
getac020 <160201919+getac020@users.noreply.github.com>
2024-11-06 19:47:47 -0600

    Update create-users.py

--- create-users.py
+++ create-users.py
@@ -1,48 +1,60 @@
 #!/usr/bin/python3
 
-#what are these imports being used for?
-import os  #Aallows python to run os commands 
-import re
-import sys
+#### Student Name: Dagim Getachew
+#### Program Name: Adduser script
+#### Program creation Date: 11/6/2024
+#### Program Last Updated Date: 11/6/2024
+
+
+import os  # Aallows python to run os commands 
+import re  # provides regular expression operations 
+import sys # provides access to system-specific parameters and functions 
 
 def main():
+
+    #loops through each line provided by the input file
     for line in sys.stdin:
 
-        #this "regular expression" is searching for the presence of a character - what is it and why?
+        #checks if the line is a comment(starts with #)
         match = re.match("^#",line)
 
-        #what is this field doing?
-        #print("The contents of the match variable were: ", match)
-        fields = line.strip().split(':') #spliting the lins into differnt fields by :
+        #splits the line into fields based on the colon separator
+        fields = line.strip().split(':') 
 
-        #print("length: ", len(fields))
 
-        #what would an appropriate comment be for describing what this IF statement is checking for?
-        #what happens if the IF statement evaluates to true?
-        #how does this IF statement rely on what happened in the prior two lines of code? The match and fields lines.
+        #skips processing for lines that are comments or are not formated properly 
         if match or len(fields) != 5:
             continue
-
+            
+        #extracting user information from the fields 
         username = fields[0]
         password = fields[1]
         gecos = "%s %s,,," % (fields[3],fields[2])
         groups = fields[4].split(',')
+
+        #creating user account with the provided GECOS field
         print("==> Creating account for %s..." % (username))
         cmd = "/usr/sbin/adduser --disabled-password --gecos '%s' %s" % (gecos,username)
-        #print cmd
-        os.system(cmd)
-        print("==> Setting the password for %s..." % (username))
-        cmd = "/bin/echo -ne '%s\n%s' | /usr/bin/sudo /usr/bin/passwd %s" % (password,password,username)
-        #print cmd
+
+        #executes commands in the systems shell
         os.system(cmd)
 
+        #setting user's password
+        print("==> Setting the password for %s..." % (username))
+        cmd = "/bin/echo -ne '%s\n%s' | /usr/bin/sudo /usr/bin/passwd %s" % (password,password,username)
+
+        #executes commands in the systems shell
+        os.system(cmd)
+
+        #assigns the user to a specified groups
         for group in groups:
-            #what is this if statement looking for?
+            #skip the dash, which indicates no group assignment
             if group != '-':
                 print("==> Assigning %s to the %s group..." % (username,group))
                 cmd = "/usr/sbin/adduser %s %s" % (username,group)
-                #print cmd
-                #os.system(cmd)
+                
+                #executes commands in the systems shell
+                os.system(cmd)
 
 if __name__ == '__main__':
     main()
